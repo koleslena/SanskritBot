95766f7a47af80d3a345c3364a6c87fbef10eaf2
Elena Kolesnikova <koleslena@yandex.ru>
2022-03-22 16:52:54 +0400

    added bot

---
+++ main.py
@@ -0,0 +1,100 @@
+from indic_transliteration import sanscript
+from indic_transliteration.sanscript import SchemeMap, SCHEMES, transliterate
+
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+import urllib.request
+import telebot
+import io
+import datetime
+import os
+import logging
+
+logger = telebot.logger
+
+formatter = '[%(asctime)s] %(levelname)8s --- %(message)s (%(filename)s:%(lineno)s)'
+logging.basicConfig(
+    filename=f'bot-from-{datetime.datetime.now().date()}.log',
+    filemode='w',
+    format=formatter,
+    datefmt='%Y-%m-%d %H:%M:%S',
+    level=logging.WARNING
+)
+
+TOKEN = os.environ.get("sansbot_token")
+
+bot = telebot.TeleBot(TOKEN)
+selectedAction = {}
+URL_MV = "https://sanskrit-lexicon.uni-koeln.de/scans/MWScan/2020/web/webtc/getword.php?key={word}&filter=roman&accent=no&transLit=hk"
+
+def gen_markup():
+    markup = InlineKeyboardMarkup()
+    markup.row_width = 2
+    markup.add(InlineKeyboardButton("TRANSLITERATE", callback_data="translit"),
+               InlineKeyboardButton("TRANSLATE", callback_data="translate"))
+    return markup
+
+def get_translit(s):
+    try:
+        return transliterate(s, sanscript.ITRANS, sanscript.DEVANAGARI);
+    except Exception as e:
+        return 'Ooopss..'
+        
+def get_translate(s):
+    try:
+        ans = urllib.request.urlopen(URL_MV.format(word=s)).read().decode("utf-8")
+        return ans[ans.find('<body>') + 6:len(ans)]
+    except Exception as e:
+        return 'Ooopss..'
+
+def get_answer(message):
+    try:
+        if selectedAction['action'] == "translit":
+            bot.send_message(message.chat.id, get_translit(message.text))
+        elif selectedAction['action'] == "translate":
+            ans = get_translate(message.text)
+            bot.send_chat_action(message.chat.id, 'upload_document')
+            with io.StringIO(ans) as doc:
+                doc.name = f"answer-{message.text}.html"
+                bot.send_document(message.chat.id, doc)
+    except Exception as e:
+        return 'Ooopss..'
+        
+# Handle '/start'
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    msg = bot.send_message(message.chat.id, f"""\
+    Hi, {message.from_user.first_name}, I am SanskritBot.
+    I can transliterate ITRANS -> DEVANAGARI \
+    and translate Sanskrit -> English (MV).
+    Choose the action \
+""", reply_markup=gen_markup())
+            
+# Handle '/help'
+@bot.message_handler(commands=['help'])
+def send_help(message):
+    msg = bot.send_message(message.chat.id, f"""\
+    I can transliterate ITRANS -> DEVANAGARI \
+    and translate Sanskrit -> English (MV) \
+    Choose the action ? \
+""", reply_markup=gen_markup())
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    try:
+        if call.data == "translit":
+            selectedAction['action'] = 'translit'
+            bot.send_message(call.from_user.id, "Send your text")
+
+        elif call.data == "translate":
+            selectedAction['action'] = 'translate'
+            bot.send_message(call.from_user.id, "Send your text")
+    except:
+        bot.send_message(call.from_user.id, 'something went wrong try again later')
+        
+# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    get_answer(message)
+    
+
+bot.polling(none_stop=True, interval=0)
