1e6d3fc75c8c56c0b707151955a425719e989afb
Elena Kolesnikova <koleslena@yandex.ru>
2022-03-27 00:35:55 +0400

    added main menu and dicts

--- main.py
+++ main.py
@@ -2,13 +2,20 @@ from indic_transliteration import sanscript, detect
 from indic_transliteration.sanscript import transliterate
 
 import telebot
-from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton
 import urllib.request
 import datetime
 import os
 import logging
 from DictParser import DictParser
 from markdown_util import parse_answer
+
+APT = "AP90"
+WIL = "WIL"
+MW = "MW"
+
+TRANSLATE = "translate"
+TRANSLIT = "translit"
 
 logger = telebot.logger
 
@@ -25,21 +32,39 @@ TOKEN = os.environ.get("sansbot_token")
 
 bot = telebot.TeleBot(TOKEN, parse_mode='HTML')
 selectedAction = {}
-URL_MV = "https://sanskrit-lexicon.uni-koeln.de/scans/MWScan/2020/web/webtc/getword.php?%s"
+URL_DICT = "https://sanskrit-lexicon.uni-koeln.de/scans/{}Scan/2020/web/webtc/getword.php?%s"
 
 
-def gen_markup():
+def gen_main_menu():
+    markup = ReplyKeyboardMarkup(True, False)
+    # markup.row_width = 2
+    markup.add(KeyboardButton("/actions"),
+               KeyboardButton("/dicts"))
+    return markup
+
+
+def gen_markup_actions():
     markup = InlineKeyboardMarkup()
     markup.row_width = 2
-    markup.add(InlineKeyboardButton("TRANSLITERATE", callback_data="translit"),
-               InlineKeyboardButton("TRANSLATE", callback_data="translate"))
+    markup.add(InlineKeyboardButton("TRANSLITERATE", callback_data=TRANSLIT),
+               InlineKeyboardButton("TRANSLATE", callback_data=TRANSLATE))
+    return markup
+
+
+def gen_markup_dicts():
+    markup = InlineKeyboardMarkup()
+    markup.row_width = 2
+    markup.add(InlineKeyboardButton("MW", callback_data=MW),
+               InlineKeyboardButton("WILSON", callback_data=WIL),
+               InlineKeyboardButton("APTE", callback_data=APT),
+               )
     return markup
 
 
 def get_translit(s):
     try:
         if detect.detect(s) == sanscript.DEVANAGARI:
-            return transliterate(s, sanscript.DEVANAGARI, sanscript.ITRANS)
+            return transliterate(s, sanscript.DEVANAGARI, sanscript.HK)
         elif detect.detect(s) == sanscript.ITRANS:
             return transliterate(s, sanscript.ITRANS, sanscript.DEVANAGARI)
         elif detect.detect(s) == sanscript.HK:
@@ -58,11 +83,14 @@ def get_lines(answer):
 
 def get_translate(s):
     try:
-        translit = 'hk'
+        translit_ = 'hk'
         if detect.detect(s) == sanscript.DEVANAGARI:
-            translit = 'deva'
-        params = urllib.parse.urlencode({'key': s, 'filter': 'roman', 'accent': 'no', 'transLit': translit})
-        url = URL_MV % params
+            translit_ = 'deva'
+        params = urllib.parse.urlencode({'key': s, 'filter': 'roman', 'accent': 'no', 'transLit': translit_})
+        dict_ = MW
+        if selectedAction.get('dict'):
+            dict_ = selectedAction['dict']
+        url = URL_DICT.format(dict_) % params
         ans = urllib.request.urlopen(url).read().decode("utf-8")
         return parse_answer(get_lines(ans), url)
     except Exception as e:
@@ -72,14 +100,30 @@ def get_translate(s):
 
 def get_answer(message):
     try:
-        if selectedAction['action'] == "translit":
+        if selectedAction['action'] == TRANSLIT:
             bot.send_message(message.chat.id, get_translit(message.text))
-        elif selectedAction['action'] == "translate":
+        elif selectedAction['action'] == TRANSLATE:
             bot.send_message(message.chat.id, get_translate(message.text))
     except Exception as e:
         logger.error(e)
-        bot.send_message(message.chat.id, "Please choose an action")
+        bot.send_message(message.chat.id, "Please choose the action")
         return 'Ooopss..'
+
+
+# Handle '/actions'
+@bot.message_handler(commands=['actions'])
+def handle_actions(message):
+    msg = bot.send_message(message.chat.id, f"""\
+    Please choose the action \
+    """, reply_markup=gen_markup_actions())
+
+
+# Handle '/dicts'
+@bot.message_handler(commands=['dicts'])
+def handle_dicts(message):
+    msg = bot.send_message(message.chat.id, f"""\
+    Please choose the dictionary \
+    """, reply_markup=gen_markup_dicts())
 
 
 # Handle '/start'
@@ -88,9 +132,9 @@ def send_welcome(message):
     msg = bot.send_message(message.chat.id, f"""\
     Hi, <i>{message.from_user.first_name}</i>, I am SanskritBot.
     I can transliterate to/from DEVANAGARI \
-    and translate Sanskrit -> English (MV).
-    Choose the action \
-    """, reply_markup=gen_markup())
+    and translate Sanskrit -> English (MW).
+    Please choose the action \
+    """, reply_markup=gen_main_menu())
 
 
 # Handle '/help'
@@ -98,20 +142,32 @@ def send_welcome(message):
 def send_help(message):
     msg = bot.send_message(message.chat.id, f"""\
     I can transliterate to/from DEVANAGARI \
-    and translate Sanskrit -> English (MV) \
-    Choose the action ? \
-""", reply_markup=gen_markup())
+    and translate Sanskrit -> English (MW) \
+    Please choose the action  \
+""", reply_markup=gen_main_menu())
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
     try:
-        if call.data == "translit":
-            selectedAction['action'] = 'translit'
+        if call.data == TRANSLIT:
+            selectedAction['action'] = TRANSLIT
             bot.send_message(call.from_user.id, "Send your text")
 
-        elif call.data == "translate":
-            selectedAction['action'] = 'translate'
+        elif call.data == TRANSLATE:
+            selectedAction['action'] = TRANSLATE
+            bot.send_message(call.from_user.id, "Send your text or choose the dictionary")
+
+        elif call.data == MW:
+            selectedAction['dict'] = MW
+            bot.send_message(call.from_user.id, "Send your text")
+
+        elif call.data == WIL:
+            selectedAction['dict'] = WIL
+            bot.send_message(call.from_user.id, "Send your text")
+
+        elif call.data == APT:
+            selectedAction['dict'] = APT
             bot.send_message(call.from_user.id, "Send your text")
     except:
         bot.send_message(call.from_user.id, 'something went wrong try again later')
